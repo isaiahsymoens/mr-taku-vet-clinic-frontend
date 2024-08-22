0ab82f0322c201abf6dabd590d55985b34e88f40
isaiahsymoens <ibsymoens.work@gmail.com>
2024-08-22 15:48:27 +0800

    initial setup and implementation of the login page

---
+++ src/pages/Login/LoginPage.tsx
@@ -0,0 +1,50 @@
+import {Box, Typography, TextField, Button} from "@mui/material";
+
+const LoginPage: React.FC = () => {
+    return <Box sx={{width: "100vw", height: "100vh", background: "#ebebeb"}}>
+        <Box sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "70vh"
+        }}>
+            <Box sx={{
+                width: "100%",
+                maxWidth: "400px",
+                height: "300px",
+                boxShadow: "10px",
+                background: "#FFF",
+                margin: {xs: "0 10px", sm: "0"},
+                borderRadius: {xs: ".5em", sm: "0"}
+            }}>
+                <Box sx={{
+                    background: "#3771d0",
+                    height: "60px",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    borderTopLeftRadius: {xs: ".5em", sm: "0"},
+                    borderTopRightRadius: {xs: ".5em", sm: "0"}
+                }}>
+                    <Typography variant="h2" sx={{ fontSize: "1.7rem", color: "#FFF", fontWeight: "700" }}>
+                        <span style={{ fontWeight: "100" }}>Mr. Taku</span>VET Clinic
+                    </Typography>
+                </Box>  
+                <form style={{
+                    maxWidth: "85%",
+                    display: "flex",
+                    flexDirection: "column",
+                    margin: "auto",
+                    gap: "20px",
+                    marginTop: "30px"
+                }}>
+                    <TextField label="Username" variant="outlined" size="small" />
+                    <TextField label="Password" variant="outlined" size="small"/>
+                    <Button variant="contained">Login</Button>
+                </form>
+            </Box>
+        </Box>
+    </Box>
+}
+
+export default LoginPage;

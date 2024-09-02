127221b77aa4b4dbb9160f926160f447dfb1a25c
isaiahsymoens <ibsymoens.work@gmail.com>
2024-09-02 13:25:52 +0800

    wip - user api

--- src/api/users.ts
+++ src/api/users.ts
@@ -7,3 +7,32 @@ export const fetchUsers = async () => {
     });
     return User.fromJSONArray((await response.json()).data as JSONObject[]);
 }
+
+export const addUser = async (data: User) => {
+    const response = await fetch("https://localhost:5001/api/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    });
+    console.log("add response :", response);
+}
+
+export const updateUser = async (data: User) => {
+    const response = await fetch("https://localhost:5001/api/users", {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    });
+    console.log("edit response :", response);
+}
+
+export const deleteUser = async (username: string) => {
+    const response = await fetch(`https://localhost:5001/api/users/${username}`, {
+        method: "DELETE"
+    });
+    console.log("delete response :", response);
+}

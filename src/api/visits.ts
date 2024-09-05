c8b3dd3aa7e16bb5909d281541769817a4ab4c46
isaiahsymoens <ibsymoens.work@gmail.com>
2024-09-06 06:49:22 +0800

    wip - edit visit

--- src/api/visits.ts
+++ src/api/visits.ts
@@ -1,7 +1,8 @@
 import {Visit} from "../models/visit";
 import {JSONObject} from "../utils/json";
 
-export interface AddVisitRequest extends Omit<Visit, "visitType"> {
+export interface AddEditVisitRequest extends Omit<Visit, "visitType"> {
+    owner: string;
     visitTypeId: number;
     petId: number;
 }
@@ -20,7 +21,7 @@ export const getPetVisits = async (id: number) => {
     return Visit.fromJSONArray((await response.json()).data as JSONObject[]);
 }
 
-export const addVisit = async (data: AddVisitRequest) => {
+export const addVisit = async (data: AddEditVisitRequest) => {
     const response = await fetch("https://localhost:5001/api/visits", {
         method: "POST",
         headers: {

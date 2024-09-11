11b055a789ee41a2354adfc410c882516ccca440
isaiahsymoens <ibsymoens.work@gmail.com>
2024-09-11 10:24:27 +0800

    wip - not found page

---
+++ src/pages/NotFound/NotFoundPage.tsx
@@ -0,0 +1,38 @@
+import React, {useEffect} from "react";
+import notFoundImg from "../../assets/images/undraw_page_not_found_re_e9o6.svg";
+import {Box} from "@mui/material";
+import {useNavigate} from "react-router-dom";
+
+const NotFoundPage = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        setTimeout(() => {
+            navigate("/users");
+        }, 3000);
+    }, []);
+
+    return (
+        <React.Fragment>
+            <Box sx={{
+                    width: "100vw", 
+                    height: "100vh",
+                    display: "flex",  
+                    justifyContent: "center"
+                }}
+            >
+                <img 
+                    src={notFoundImg} 
+                    alt="404 Not Found"
+                    style={{
+                        width: "100%",
+                        maxWidth: "860px",
+                        marginTop: "-100px"
+                    }}    
+                />
+            </Box>
+        </React.Fragment>
+    );
+}
+
+export default NotFoundPage;

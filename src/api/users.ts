3390dafaca7a563e0d22dfbe1e5a9d55cb90b02b
isaiahsymoens <ibsymoens.work@gmail.com>
2024-09-19 15:49:14 +0800

    wip - login page

--- src/api/users.ts
+++ src/api/users.ts
@@ -1,5 +1,6 @@
 import {PaginatedResponse} from "../models/paginatedResponse";
 import {User} from "../models/user";
+import {LoginDetails} from "../pages/Login/LoginPage";
 import {extractErrorMessages, GenericErrorResponse} from "../utils/errorHelper";
 import {JSONObject} from "../utils/json";
 
@@ -41,6 +42,21 @@ export const searchUsersByName = async (name: string, sortBy?: string, isAscendi
 
 export const addUser = async (data: AddEditUserRequest) => {
     const response = await fetch(`https://localhost:5001/api/users`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    });
+    const json = await response.json();
+    if (!response.ok) {
+        throw extractErrorMessages(json.errors as GenericErrorResponse);
+    }
+    return User.fromJSON(json.data as JSONObject);
+}
+
+export const loginUser = async (data: LoginDetails) => {
+    const response = await fetch(`https://localhost:5001/api/users/login`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json"

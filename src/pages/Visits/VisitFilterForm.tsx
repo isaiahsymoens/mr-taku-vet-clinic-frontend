fa07ffeb9a837418079bc07ce7b985ac30788775
isaiahsymoens <ibsymoens.work@gmail.com>
2024-09-23 10:14:45 +0800

    wip - visit filter dropdown

--- src/pages/Visits/VisitFilterForm.tsx
+++ src/pages/Visits/VisitFilterForm.tsx
@@ -1,8 +1,12 @@
-import React from "react";
-import {Box, Button, InputBase, Typography} from "@mui/material";
+import React, {useEffect, useState} from "react";
+import {Box, Button, InputBase, MenuItem, Select, Typography} from "@mui/material";
 import {DatePicker, LocalizationProvider} from "@mui/x-date-pickers";
 import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
 import {Dayjs} from "dayjs";
+import {PetType} from "../../models/petType";
+import {VisitType} from "../../models/visitType";
+import {getPetTypes} from "../../api/petTypes";
+import {getVisitTypes} from "../../api/visitTypes";
 
 export interface VisitFilterModel {
     firstName?: string;
@@ -22,6 +26,17 @@ type VisitFilterProps = {
 }
 
 const VisitFilter: React.FC<VisitFilterProps> = ({onSearch, visitForm, onFormChangeVisitFilter, onClearVisitFilter}) => {
+    const [petTypes, setPetTypes] = useState<PetType[]>([]);
+    const [visitTypes, setVisitTypes] = useState<VisitType[]>([]);
+
+    useEffect(() => {
+        const loadData = async () => {
+            setPetTypes(await getPetTypes());
+            setVisitTypes(await getVisitTypes());
+        }
+        loadData();
+    }, []);
+
     return (
         <React.Fragment>
             <Box sx={{width: "100%", minWidth: "350px", maxWidth: "370px", px: 3, py: 2, mt: 2}}>
@@ -77,33 +92,89 @@ const VisitFilter: React.FC<VisitFilterProps> = ({onSearch, visitForm, onFormCha
                     <Typography variant="subtitle2" sx={{width: "110px", whiteSpace: "nowrap", color: "#5F6368", fontSize: "14px", fontWeight: 100}}>
                         Pet Type:
                     </Typography>
-                    <InputBase
-                        id="petType"
+                    <Select
                         sx={{
-                            borderBottom: "0.5px solid #5F6368",
                             width: "100%",
                             fontSize: ".9rem",
-                            input: {padding: 0}
-                        }}
+                            px: 0,
+                            boxShadow: "none",
+                            ".MuiOutlinedInput-notchedOutline": { 
+                                border: 0,
+                                borderBottom: "1px solid #000",
+                                borderRadius: "0px"
+                            },
+                            "&.MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline": {
+                                border: 0,
+                                borderBottom: "1px solid #000",
+                                borderRadius: "0px",   
+                            },
+                            "&.MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                                border: 0,
+                                borderBottom: "1px solid #000",
+                                borderRadius: "0px",   
+                            },
+                            "& .MuiSelect-select": {
+                                p: 0,
+                                fontSize: ".9rem",
+                            }
+                          }}
                         value={visitForm?.petType}
-                        onChange={(e) => onFormChangeVisitFilter("petType", e.target.value)}
-                    />
+                        onChange={(e) => onFormChangeVisitFilter("petType", e.target.value)} 
+                    >
+                        {petTypes.map(pType => 
+                            <MenuItem 
+                                key={pType.petTypeId} 
+                                value={pType.typeName}
+                                sx={{fontSize: ".9rem"}}
+                            >
+                                {pType.typeName}
+                            </MenuItem>
+                        )}
+                    </Select>
                 </Box>
                 <Box sx={{display: "flex", alignItems: "center", mb: 1}}>
                     <Typography variant="subtitle2" sx={{width: "110px", whiteSpace: "nowrap", color: "#5F6368", fontSize: "14px", fontWeight: 100}}>
                         Visit Type:
                     </Typography>
-                    <InputBase
-                        id="visitType"
+                    <Select
                         sx={{
-                            borderBottom: "0.5px solid #5F6368",
                             width: "100%",
                             fontSize: ".9rem",
-                            input: {padding: 0}
-                        }}
+                            px: 0,
+                            boxShadow: "none",
+                            ".MuiOutlinedInput-notchedOutline": { 
+                                border: 0,
+                                borderBottom: "1px solid #000",
+                                borderRadius: "0px"
+                            },
+                            "&.MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline": {
+                                border: 0,
+                                borderBottom: "1px solid #000",
+                                borderRadius: "0px",   
+                            },
+                            "&.MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                                border: 0,
+                                borderBottom: "1px solid #000",
+                                borderRadius: "0px",   
+                            },
+                            "& .MuiSelect-select": {
+                                p: 0,
+                                fontSize: ".9rem",
+                            }
+                          }}
                         value={visitForm?.visitType}
-                        onChange={(e) => onFormChangeVisitFilter("visitType", e.target.value)}
-                    />
+                        onChange={(e) => onFormChangeVisitFilter("visitType", e.target.value)} 
+                    >
+                        {visitTypes.map(vType => 
+                            <MenuItem 
+                                key={vType.visitTypeId} 
+                                value={vType.typeName}
+                                sx={{fontSize: ".9rem"}}
+                            >
+                                {vType.typeName}
+                            </MenuItem>
+                        )}
+                    </Select>
                 </Box>
                 <Box sx={{display: "flex", alignItems: "center", mb: 1}}>
                     <Typography variant="subtitle2" sx={{width: "110px", whiteSpace: "nowrap", color: "#5F6368", fontSize: "14px", fontWeight: 100}}>
